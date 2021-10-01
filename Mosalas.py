69044aa606605ce47cd168a8202dde55e0478805
AlirezaZP <91659986+AlirezaZP@users.noreply.github.com>
2021-10-01 17:39:16 +0330

    Add files via upload

---
+++ Mosalas.py
@@ -0,0 +1,7 @@
+z1=float(input("Zele 1 ra vared konid: "))
+z2=float(input("Zele 2 ra vared konid: "))
+z3=float(input("Zele 3 ra vared konid: "))
+if z1 + z2 > z3 and z1 + z3 > z2 and z2 + z3 > z1:
+    print("Mosalas tashkil mishe")
+else:
+    print("Mosalas tashkil nmishe")
